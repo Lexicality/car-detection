267adea557d277f46a2d3e41d38da9913ea828eb
Lex Robinson <lexi@lexi.org.uk>
2015-12-15 18:57:31 +0000

    Restructure the IRC logic into its own file

---
+++ irc.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sorcix/irc"
+)
+
+type Session struct {
+	*irc.Conn
+	// Set this bit on construction
+	Server   string
+	Port     int
+	UserName string
+	NickName string
+	RealName string
+	// Set by the internals
+	messages <-chan *irc.Message
+}
+
+func (session *Session) Dial() error {
+	conn, err := irc.Dial(fmt.Sprintf("%s:%d", session.Server, session.Port))
+	if err != nil {
+		return err
+	}
+	session.Conn = conn
+	return session.handshake()
+}
+
+func (session *Session) handshake() (err error) {
+	err = session.Encode(&irc.Message{
+		Command: "NICK",
+		Params:  []string{session.NickName},
+	})
+	if err != nil {
+		return err
+	}
+	return session.Encode(&irc.Message{
+		Command:  "USER",
+		Params:   []string{session.UserName, "0", "*"},
+		Trailing: session.RealName,
+	})
+}
+
+func (session *Session) Privmsg(name, message string) error {
+	return session.Encode(&irc.Message{
+		Command:  "PRIVMSG",
+		Params:   []string{name},
+		Trailing: message,
+	})
+}
+
+func (session *Session) handlePing(message *irc.Message) (err error) {
+	// Ha ha ha this is so dodgy
+	message.Command = "PONG"
+	return session.Encode(message)
+}
+
+func (session *Session) readPump() (err error) {
+	var toIgnore = [...]string{"001", "002", "003", "005", "251", "252", "254", "255", "265", "266"}
+	var shouldIgnore = make(map[string]bool, len(toIgnore))
+	for _, num := range toIgnore {
+		shouldIgnore[num] = true
+	}
+	var m *irc.Message
+
+	for {
+		m, err = session.Decode()
+		if err != nil {
+			return err
+		}
+		// Ignore informative spam
+		if shouldIgnore[m.Command] {
+			continue
+		}
+		// Start of glorious message type switches
+		if m.Command == "PING" {
+			err = session.handlePing(m)
+			if err != nil {
+				log.Error("Couldn't pong: %s", err)
+			}
+		} else if m.Command == "004" {
+			// The server sends 001 002 003 and 004 in fast succession.
+			// It's probably a good idea to wait until #4 happens before doing anything
+			log.Info("Connection established")
+		} else if m.Command == "376" || m.Command == "422" {
+			// MOTD is finished, start harassing people
+			// log.Info("MOTD Complete, fully connected!")
+		} else {
+			log.Info("Got message: %+v", m)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,19 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/op/go-logging"
-	"github.com/sorcix/irc"
 	"io"
 	"io/ioutil"
 	"strings"
-)
-
-const (
-	server   = "irc.mindfang.org"
-	port     = 6667
-	username = "pcc31"
-	nickname = "randomTesting"
 )
 
 var log = logging.MustGetLogger("Random Encounters")
@@ -26,82 +17,29 @@ func getNSPass() (string, error) {
 	return strings.TrimSpace(string(contents)), nil
 }
 
-func handshake(conn *irc.Conn) (err error) {
-	err = conn.Encode(&irc.Message{
-		Command: "NICK",
-		Params:  []string{nickname},
-	})
-	if err != nil {
-		return err
-	}
-	return conn.Encode(&irc.Message{
-		Command:  "USER",
-		Params:   []string{username, "0", "*"},
-		Trailing: "Hello world",
-	})
-}
-
-func handlePing(conn *irc.Conn, message *irc.Message) (err error) {
-	// Ha ha ha this is so dodgy
-	message.Command = "PONG"
-	return conn.Encode(message)
-}
-
-func readPump(conn *irc.Conn) (err error) {
-	var toIgnore = [...]string{"001", "002", "003", "005", "251", "252", "254", "255", "265", "266"}
-	var shouldIgnore = make(map[string]bool, len(toIgnore))
-	for _, num := range toIgnore {
-		shouldIgnore[num] = true
-	}
-	var m *irc.Message
-
-	for {
-		m, err = conn.Decode()
-		if err != nil {
-			return err
-		}
-		// Ignore informative spam
-		if shouldIgnore[m.Command] {
-			continue
-		}
-		// Start of glorious message type switches
-		if m.Command == "PING" {
-			err = handlePing(conn, m)
-			if err != nil {
-				log.Error("Couldn't pong: %s", err)
-			}
-		} else {
-			log.Info("Got message: %+v", m)
-		}
-	}
-}
-
 func main() {
-	fmt.Println("Hello world")
-	// nspass, err := getNSPass()
-	// if err != nil {
-	// 	log.Fatalf("Unable to get Nickserv password: %s", err)
-	// }
-	// db stuff probably
-	conn, err := irc.Dial(fmt.Sprintf("%s:%d", server, port))
+	var err error
+
+	var session = &Session{
+		Server:   "irc.mindfang.org",
+		Port:     6667,
+		UserName: "pcc31",
+		NickName: "randomTesting",
+		RealName: "HI MOPM",
+	}
+
+	err = session.Dial()
 	if err != nil {
 		log.Fatalf("Could not connect to the server: %s", err)
 	}
-	// say hello
-	err = handshake(conn)
-	if err != nil {
-		conn.Close()
-		log.Fatalf("Could not handshake: %s", err)
-	}
+	defer session.Close()
 
 	// Blocking
-	err = readPump(conn)
+	err = session.readPump()
 
 	if err == io.EOF {
 		log.Info("Server hung up")
 	} else if err != nil {
 		log.Critical("Could not read message: %s", err)
 	}
-
-	conn.Close()
 }
